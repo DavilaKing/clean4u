4b95316158d9b35d79dab586951f76aa61b86f0e
RogueSHD <67354156+RogueSHD@users.noreply.github.com>
2021-02-09 22:34:19 +0800

    back to normal

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'CLEAN4',
+      title: 'CLEAN4U',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.blue,
